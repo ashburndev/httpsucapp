69607a702f05a4061779155d6985ec24b0d38df3
ashburndev <ashburndev@gmail.com>
2024-07-16 09:43:35 -0400

    update HttpsucappApplication.java

--- readme.txt
+++ readme.txt
@@ -50,3 +50,64 @@ C:.
                 \---httpsucapp
                         HttpsucappApplicationTests.java
                         
+C:\Users\DavidHolberton\sbprojs\httpsucapp>
+C:\Users\DavidHolberton\sbprojs\httpsucapp>.\gradlew --version
+
+------------------------------------------------------------
+Gradle 8.8
+------------------------------------------------------------
+
+Build time:   2024-05-31 21:46:56 UTC
+Revision:     4bd1b3d3fc3f31db5a26eecb416a165b8cc36082
+
+Kotlin:       1.9.22
+Groovy:       3.0.21
+Ant:          Apache Ant(TM) version 1.10.13 compiled on January 4 2023
+JVM:          17.0.7 (Oracle Corporation 17.0.7+8-LTS-224)
+OS:           Windows 11 10.0 amd64
+
+C:\Users\DavidHolberton\sbprojs\httpsucapp>
+
+cd C:\Users\DavidHolberton\sbprojs\httpsucapp
+.\gradlew clean
+.\gradlew bootJar
+dir /o:gd build
+dir /o:gd build\libs
+java -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar
+java -Djavax.net.debug=ssl:handshake -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar
+java -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar > httpsucapp-startspringio-20240716a.txt
+java -Djavax.net.debug=ssl:handshake -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar > httpsucapp-startspringio-20240716b.txt
+java -Djavax.net.debug=ssl:handshake -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar 2> httpsucapp-startspringio-20240716c.txt
+java -Djavax.net.debug=ssl:handshake -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar 2>&1 httpsucapp-startspringio-20240716d.txt
+java -Djavax.net.debug=ssl:handshake -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar > httpsucapp-startspringio-20240716e.txt 2>&1
+java -Dorg.asynchttpclient.enabledProtocols=TLSv1.3 -Dorg.asynchttpclient.enabledCipherSuites=TLS_AES_256_GCM_SHA384 -Djavax.net.debug=ssl:handshake -jar build\libs\httpsucapp-0.0.1-SNAPSHOT.jar >httpsucapp-startspringio-20240716f.txt 2>&1
+
+C:\Users\DavidHolberton\sbprojs\httpsucapp>dir /o:gd
+ Volume in drive C is OS
+ Volume Serial Number is 38E4-68D8
+
+ Directory of C:\Users\DavidHolberton\sbprojs\httpsucapp
+
+06/30/2024  09:02 AM    <DIR>          gradle
+06/30/2024  09:02 AM    <DIR>          src
+07/14/2024  08:45 PM    <DIR>          ..
+07/16/2024  08:55 AM    <DIR>          .gradle
+07/16/2024  09:19 AM    <DIR>          build
+07/16/2024  09:25 AM    <DIR>          .
+06/30/2024  09:02 AM             2,918 gradlew.bat
+06/30/2024  09:02 AM                32 settings.gradle
+06/30/2024  09:02 AM               444 .gitignore
+06/30/2024  09:02 AM             8,706 gradlew
+06/30/2024  09:02 AM             2,414 HELP.md
+07/14/2024  02:21 PM             1,199 build.gradle
+07/16/2024  09:23 AM             6,891 httpsucapp-startspringio-20240716a.txt
+07/16/2024  09:23 AM             6,891 httpsucapp-startspringio-20240716b.txt
+07/16/2024  09:24 AM            25,820 httpsucapp-startspringio-20240716c.txt
+07/16/2024  09:25 AM            34,522 httpsucapp-startspringio-20240716e.txt
+07/16/2024  09:25 AM            34,511 httpsucapp-startspringio-20240716f.txt
+07/16/2024  09:29 AM             3,271 readme.txt
+              12 File(s)        127,619 bytes
+               6 Dir(s)  484,259,897,344 bytes free
+
+C:\Users\DavidHolberton\sbprojs\httpsucapp>
+
--- src/main/java/ashburncode/httpsucapp/HttpsucappApplication.java
+++ src/main/java/ashburncode/httpsucapp/HttpsucappApplication.java
@@ -3,11 +3,92 @@ package ashburncode.httpsucapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @SpringBootApplication
 public class HttpsucappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HttpsucappApplication.class, args);
+  private static final String USER_AGENT = "Mozilla/5.0";
+  // private static final String GET_URL =
+  // "https://localhost:9090/SpringMVCExample";
+  private static final String GET_URL = "https://start.spring.io/";
+  private static final String POST_URL = "https://localhost:9090/SpringMVCExample/home";
+  private static final String POST_PARAMS = "userName=Pankaj";
+
+  public static void main(String[] args) {
+    SpringApplication.run(HttpsucappApplication.class, args);
+    try {
+      String response = createClientMakeGetGetuest(GET_URL);
+      System.out.println("response = " + response);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      System.out.println("GET DONE");
+    }
+
+    // sendPOST();
+    // System.out.println("POST DONE");
+  }
+
+  // https://www.digitalocean.com/community/tutorials/java-httpurlconnection-example-java-http-request-get-post
+
+  private static String createClientMakeGetGetuest(String getUrl) throws IOException {
+		System.out.println("createClientMakeGetGetuest getUrl = " + getUrl);
+		URL obj = new URL(GET_URL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code = " + responseCode);
+		String response = "";
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer responseBuffer = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				responseBuffer.append(inputLine);
+			}
+			in.close();
+			response = responseBuffer.toString();
+		} else {
+			response = "createClientMakeGetRequest did not work.";
+		}
+    return response;
 	}
+
+  private static void createClientMakePostGetuest() throws IOException {
+    URL obj = new URL(POST_URL);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    con.setRequestMethod("POST");
+    con.setRequestProperty("User-Agent", USER_AGENT);
+    // For POST only - START
+    con.setDoOutput(true);
+    OutputStream os = con.getOutputStream();
+    os.write(POST_PARAMS.getBytes());
+    os.flush();
+    os.close();
+    // For POST only - END
+    int responseCode = con.getResponseCode();
+    System.out.println("POST Response Code = " + responseCode);
+    if (responseCode == HttpURLConnection.HTTP_OK) { // success
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      String inputLine;
+      StringBuffer response = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+      }
+      in.close();
+      // print result
+      System.out.println(response.toString());
+    } else {
+      System.out.println("POST request did not work.");
+    }
+  }
 
 }
